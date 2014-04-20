01acbb8af8db715bf8675820c2ea7aecee94862a
namoshika <namoshika@users.noreply.github.com>
2014-04-20 20:38:53 +0900

    添付アルバムで例外が発生するバグを修正

--- SnkLib.MainLibrary/Web.GooglePlus/Primitive/Activity/AttachedAlbum.cs
+++ SnkLib.MainLibrary/Web.GooglePlus/Primitive/Activity/AttachedAlbum.cs
@@ -36,7 +36,7 @@ namespace SunokoLibrary.Web.GooglePlus.Primitive
         protected void ParseAlbum(JArray json)
         {
             //アルバム
-            var albumTitle = Primitive.ApiAccessorUtility.DecodeHtmlText((string)json[2]);
+            var albumTitle = (string)json[2];
             var albumId = (string)json[37];
             var ownerId = (string)json[26];
             Album = new AlbumData(albumId, albumTitle, LinkUrl, owner: new ProfileData(ownerId), loadedApiTypes: AlbumUpdateApiFlag.Base);
